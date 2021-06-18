45ab0fa8c068283f229d6904c15d220fd3d7d18b
Howard <howardl93@yahoo.com>
2021-06-18 13:10:38 -0700

    removed constatns

--- constants/constants.tsx
+++
@@ -1,6 +0,0 @@
-export const autoURL = 'https://maps.googleapis.com/maps/api/place/autocomplete/xml?input=Amoeba&types=establishment&location=37.76999,-122.44696&radius=500&key=YOUR_API_KEY';
-
-"https://maps.googleapis.com/maps/api/place/autocomplete/output?parameters"
-
-
-export const pass = "AIzaSyBREQ88BGn7CQbJSXGOQTTSGq4_YwWTMZI"
